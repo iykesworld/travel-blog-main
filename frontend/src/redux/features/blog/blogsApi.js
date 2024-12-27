c2ef1664313198cfb86421692b4adf4bc583a37e
iykesworld <kennethiyke12@gmail.com>
2024-12-27 23:52:37 +0000

    NODE_env added

--- frontend/src/redux/features/blog/blogsApi.js
+++ frontend/src/redux/features/blog/blogsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const blogApi = createApi({
     reducerPath: 'blogApi',
     baseQuery: fetchBaseQuery({ 
-        baseUrl: 'https://travel-blog-server-opal.vercel.app/api',
+        baseUrl: 'https://travel-blog-server-opal.vercel.app/api/',
         credentials: 'include',
         prepareHeaders: (headers, { getState }) => {
           const token = getState().auth?.token;
