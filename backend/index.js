6dc0f5956d1f256b470d4249f9a0346c45d6ed66
iykesworld <kennethiyke12@gmail.com>
2024-12-27 22:55:55 +0000

    url changed

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,8 @@ app.use(cookieParser());
 app.use(bodyParser.json({limit: '10mb'}));
 app.use(bodyParser.urlencoded({ limit: "10mb",extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://travel-blog-sigma-two.vercel.app'],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true
 }));
 
