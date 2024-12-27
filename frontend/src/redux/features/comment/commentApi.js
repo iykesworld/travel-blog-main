af3f8f9840b81b3c501ce06753be432f3ad5eff2
iykesworld <kennethiyke12@gmail.com>
2024-12-27 22:49:58 +0000

    url changed

--- frontend/src/redux/features/comment/commentApi.js
+++ frontend/src/redux/features/comment/commentApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const commentApi = createApi({
     reducerPath: "commentApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000/api/comments",
+        baseUrl: "https://travel-blog-server-opal.vercel.app/api/comments",
         credentials: "include",
     }),
     tagTypes: ["Comments"],
